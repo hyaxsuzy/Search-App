8d6f6cde2ba9e1b35934bf56d0d733d1aad19252
hyaxsuzy <91564952+hyaxsuzy@users.noreply.github.com>
2021-10-19 11:28:39 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import SearchApp from './SearchApp';
+
+
+function App() {
+  return (
+    <div className = "App">
+      <SearchApp />
+    </div>
+  );
+}
+
+export default App;
